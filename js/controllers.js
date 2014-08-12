100742c86ddf96217ded79f24b2d14f7f5dbe1a1
rick_barkley <rick_barkley@nuvola-networks.com>
2014-08-12 12:14:12 -0500

    initial push

---
+++ js/controllers.js
@@ -0,0 +1,12 @@
+var stockApp = angular.module('stockApp', []);
+
+stockApp.controller('stockListCtrl', function ($scope) {
+  $scope.stocks = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
